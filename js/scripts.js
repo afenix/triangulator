cd7183d3625dc2e44162770a88bcd5c5b22f74a5
Alister A. Fenix <a.fenix@icloud.com>
2015-02-13 11:17:18 -0800

    will determine if all sides are equal to each other.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var triangleType = function(side1, side2, side3) {
   if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
     var results = "Not a triangle";
+  } else if (side1 === side2 && side2 === side3) {
+    var results = "Equilateral triangle";
   } else if (side1 === side2 || side1 === side3 || side2 === side3) {
     var results = "Scalene triangle";
   } else {
--- specs/specs.js
+++ specs/specs.js
@@ -6,4 +6,8 @@ describe("triangleType", function() {
   it("will determine if any two sides are equal to each other", function() {
     expect(triangleType(3, 3, 5)).to.equal("Scalene triangle");
   });
+
+  it("will determine if all sides are equal to each other", function() {
+    expect(triangleType(5, 5, 5)).to.equal("Equilateral triangle");
+  })
 });
